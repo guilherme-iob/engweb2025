43502564b5ede9f6b5bc40f603a60364f689d575
leopellizzoni <lpellizzoni@ucs.br>
2025-05-10 10:19:02 -0300

    teste com about 2

--- CursoSabado/CursoSabado.Web/Controllers/HomeController.cs
+++ CursoSabado/CursoSabado.Web/Controllers/HomeController.cs
@@ -32,11 +32,18 @@ namespace CursoSabado.Web.Controllers
             return View(pessoasQueVaoParaTela);
         }
 
+        //About2 é a tela cshtml
+        //About é o caminho que fica na URL -> Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            var modeloParaTela = new InformacaoModel();
+            modeloParaTela.CodigoCurso = "ABCD";
+            modeloParaTela.Nome = "Curso ABCD";
+            modeloParaTela.Observacoes = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+            return View("About2", modeloParaTela);
         }
 
         public ActionResult Contact()
---
+++ CursoSabado/CursoSabado.Web/Models/InformacaoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoSabado.Web.Models
+{
+    public class InformacaoModel
+    {
+        public String CodigoCurso { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Observacoes { get; set; }
+
+    }
+}
