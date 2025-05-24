503c81d7ab382afc64cf54d9acf63d331a971d97
leopellizzoni <lpellizzoni@ucs.br>
2025-05-24 09:42:04 -0300

    cria a estrutura de exemplo do projeto em adonet para acesso ao banco de dados

---
+++ CursoSabado/CursoSabado.Repositorios.ADONET/Pessoas/RepositorioDePessoaADONET.cs
@@ -0,0 +1,28 @@
+﻿using CursoSabado.Dominio.Pessoas;
+using CursoSabado.Repositorios.Pessoas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoSabado.Repositorios.ADONET.Pessoas
+{
+    public class RepositorioDePessoaADONET : IRepositorioDePessoa
+    {
+        public Pessoa? Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Pessoa> ObterPorInicioDeNome(string conteudo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Pessoa> OterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
